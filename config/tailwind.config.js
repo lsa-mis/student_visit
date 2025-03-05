382d342d18a68d3fe2c962811d4bf7a748fe1fc7
rsmokeUM <rsmoke@umich.edu>
2025-03-05 13:54:03 -0500

    Simplify Tailwind CSS configuration
    Remove extended theme customizations and default plugins, converting the configuration to a minimal ESM export

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,35 +1,9 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
-module.exports = {
+export default {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        'um': {
-          'blue': '#00274C',      // Example Michigan blue
-          'maize': '#FFCB05',     // Example Michigan yellow/maize
-          'neutral': '#989C97'    // Example neutral color
-        },
-        // You can add more custom colors:
-        'lsa': {
-          'primary': '#2F65A7',
-          'secondary': '#333333',
-          // Add more as needed
-        }
-      }
-    }
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
+  plugins: [],
 }
