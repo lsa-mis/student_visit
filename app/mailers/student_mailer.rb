f1c8afcc28148b5e4280af9a9a451a35a8008eb6
rsmokeUM <rsmoke@umich.edu>
2026-02-05 19:43:10 -0500

    Refactor mailer classes to use mail options hash for improved readability and maintainability. Updated footer in mailer layout to remove redundant reply instructions.

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -7,9 +7,14 @@ class StudentMailer < ApplicationMailer
     # Attach LSA logo for email
     attachments.inline["LSA_Logo.svg"] = File.read(Rails.root.join("app", "assets", "images", "LSA_Logo.svg"))
 
-    mail(
+    reply_to = @program&.information_email_address.presence
+
+    mail_options = {
       to: user.email_address,
       subject: "Welcome to #{program.name}"
-    )
+    }
+    mail_options[:reply_to] = reply_to if reply_to
+
+    mail(mail_options)
   end
 end
