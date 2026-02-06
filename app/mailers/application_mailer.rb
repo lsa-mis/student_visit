6146731440f151b031888d11cd60ad5e1a3e2225
rsmokeUM <rsmoke@umich.edu>
2026-02-05 19:53:31 -0500

    Add LSA logo to email templates and refactor mailer classes
    - Introduced a new LSA logo image (LSA_Logo.png) for use in email communications.
    - Updated ApplicationMailer to attach the LSA logo inline for emails.
    - Removed the previous attachment of LSA_Logo.svg from StudentMailer.
    - Modified mailer layout to display the LSA logo, prioritizing the PNG format over SVG.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,17 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "no-reply@student-visit.lsa.umich.edu"
   layout "mailer"
+
+  before_action :attach_inline_lsa_logo
+
+  private
+
+  def attach_inline_lsa_logo
+    return if attachments.inline["LSA_Logo.png"].present?
+
+    attachments.inline["LSA_Logo.png"] = {
+      mime_type: "image/png",
+      content: File.binread(Rails.root.join("app", "assets", "images", "LSA_Logo.png"))
+    }
+  end
 end
--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -4,9 +4,6 @@ class StudentMailer < ApplicationMailer
     @program = program
     @is_new_student = is_new_student
 
-    # Attach LSA logo for email
-    attachments.inline["LSA_Logo.svg"] = File.read(Rails.root.join("app", "assets", "images", "LSA_Logo.svg"))
-
     reply_to = @program&.information_email_address.presence
 
     mail_options = {
