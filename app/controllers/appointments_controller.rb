72b37930959a93fa2876e756907601df4392c94f
rsmokeUM <rsmoke@umich.edu>
2026-03-18 12:02:16 -0400

    Refactor appointment creation permission check in index view
    Updated the appointments index view to use the instance variable @can_create_appointments for determining if the user can create new appointments. This change enhances readability by removing direct policy calls from the view. Additionally, the appointments controller was updated to set this variable based on the user's permissions.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -6,6 +6,7 @@ class AppointmentsController < ApplicationController
     @appointments = @program.appointments.includes(:vip, :student).order(:start_time)
     authorize Appointment.new(program: @program)
     @can_view_appointments = policy(Appointment.new(program: @program)).show?
+    @can_create_appointments = policy(Appointment.new(program: @program)).create?
   end
 
   def show
